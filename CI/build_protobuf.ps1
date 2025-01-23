a7e32726f70ea6da039e6baa7de6a0d068cab464
Seif Kandil <74428638+k0T0z@users.noreply.github.com>
2025-01-23 22:50:00 +0000

    Fixed workflows

--- CI/build_protobuf.ps1
+++ CI/build_protobuf.ps1
@@ -1,63 +1,32 @@
 param(
-    [string]$absl_tag,
-    [string]$protobuf_tag,
-    [string]$absl_prefix,
     [string]$protobuf_prefix
 )
+
+$PROTOBUF_LATEST_TAG = ($(git ls-remote --tags https://github.com/protocolbuffers/protobuf.git | Select-Object -Last 1) -split '\s+')[1].Split('/')[-1].Split('^')[0]
 
 $ErrorActionPreference = 'Stop'
 
 # Validate parameters
-if (-not $absl_tag) { throw "Abseil tag parameter is required" }
-if (-not $protobuf_tag) { throw "Protobuf tag parameter is required" }
-if (-not $absl_prefix) { throw "Abseil install prefix is required" }
 if (-not $protobuf_prefix) { throw "Protobuf install prefix is required" }
 
-# Build and install Abseil
-Write-Output "Installing Abseil $absl_tag..."
-git clone --depth 1 -b $absl_tag https://github.com/abseil/abseil-cpp.git
-cd abseil-cpp
-
-mkdir build
-cd build
-
-cmake .. -G "Visual Studio 17 2022" `
-         -DABSL_PROPAGATE_CXX_STD=ON `
-         -DBUILD_TESTING=OFF `
-         -DABSL_BUILD_TESTING=OFF `
-         -DABSL_USE_GOOGLETEST_HEAD=OFF `
-         -DCMAKE_BUILD_TYPE=Release `
-         -DCMAKE_CXX_STANDARD=17 `
-         -DABSL_ENABLE_INSTALL=ON `
-         -DBUILD_SHARED_LIBS=ON `
-         -DABSL_BUILD_MONOLITHIC_SHARED_LIBS=ON `
-         -DCMAKE_INSTALL_PREFIX="$absl_prefix"
-
-cmake --build . --config Release --parallel
-cmake --install . --config Release
-
-cd $env:GITHUB_WORKSPACE
-
 # Build and install Protobuf
-Write-Output "Installing Protobuf $protobuf_tag..."
-git clone --depth 1 -b $protobuf_tag https://github.com/protocolbuffers/protobuf.git
-cd protobuf
+Write-Output "Installing Protobuf $PROTOBUF_LATEST_TAG..."
+git clone --depth 1 -b $PROTOBUF_LATEST_TAG https://github.com/protocolbuffers/protobuf.git
+Set-Location protobuf
 
 git submodule update --init --recursive
 
-mkdir build
-cd build
+New-Item -ItemType Directory -Path build | Set-Location
 
 cmake .. -G "Visual Studio 17 2022" `
          -Dprotobuf_BUILD_TESTS=OFF `
-         -Dprotobuf_ABSL_PROVIDER=package `
+         -Dprotobuf_ABSL_PROVIDER=module `
          -DCMAKE_BUILD_TYPE=Release `
          -DCMAKE_CXX_STANDARD=17 `
          -Dprotobuf_BUILD_SHARED_LIBS=ON `
-         -DCMAKE_PREFIX_PATH="$absl_prefix" `
          -DCMAKE_INSTALL_PREFIX="$protobuf_prefix"
 
 cmake --build . --config Release --parallel
 cmake --install . --config Release
 
-cd $env:GITHUB_WORKSPACE
+Set-Location $env:GITHUB_WORKSPACE
