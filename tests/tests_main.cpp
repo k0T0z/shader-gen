ec68bbee5a2882f9e23bac32f42a6d6d25aced36
Seif Kandil <74428638+k0T0z@users.noreply.github.com>
2025-03-05 22:00:13 +0000

    Added ui tests, but will add it later heh

---
+++ tests/tests_main.cpp
@@ -0,0 +1,48 @@
+/*********************************************************************************/
+/*                                                                               */
+/*  Copyright (C) 2024 Seif Kandil (k0T0z)                                       */
+/*                                                                               */
+/*  This file is a part of the ENIGMA Development Environment.                   */
+/*                                                                               */
+/*                                                                               */
+/*  ENIGMA is free software: you can redistribute it and/or modify it under the  */
+/*  terms of the GNU General Public License as published by the Free Software    */
+/*  Foundation, version 3 of the license or any later version.                   */
+/*                                                                               */
+/*  This application and its source code is distributed AS-IS, WITHOUT ANY       */
+/*  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS    */
+/*  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more        */
+/*  details.                                                                     */
+/*                                                                               */
+/*  You should have recieved a copy of the GNU General Public License along      */
+/*  with this code. If not, see <http://www.gnu.org/licenses/>                   */
+/*                                                                               */
+/*  ENIGMA is an environment designed to create games and other programs with a  */
+/*  high-level, fully compilable language. Developers of ENIGMA or anything      */
+/*  associated with ENIGMA are in no way responsible for its users or            */
+/*  applications created by its users, or damages caused by the environment      */
+/*  or programs made in the environment.                                         */
+/*                                                                               */
+/*********************************************************************************/
+
+#include <QApplication>
+#include <QtTest/QtTest>
+#include <gtest/gtest.h>
+
+#include "tests/gui/controller/test_visual_shader_editor.hpp"
+
+int main(int argc, char** argv) {
+    // Initialize Google Test
+    // Run Google Test tests.
+    ::testing::InitGoogleTest(&argc, argv);
+    int gtest_result = RUN_ALL_TESTS();
+
+    QApplication app(argc, argv);
+
+    // Run Qt tests.
+    VisualShaderEditorTest qtTest;
+    int qt_result = QTest::qExec(&qtTest, argc, argv);
+
+    // Combine the results.
+    return (gtest_result == 0 && qt_result == 0) ? 0 : 1;
+}
